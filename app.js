9ddcb99f3fc5284a9fe8fe8f95a45adc8bc187f8
Vyacheslav Feofanov <slkf72@yandex.ru>
2018-04-04 18:57:33 +0700

    Added writing data to file

--- app.js
+++ app.js
@@ -10,10 +10,20 @@ const readData = (filepath) => {
     }
     catch(err){console.log(err);}
 };
+const writeData = (filepath, data) => {
 
-let filepath = './input.txt';
-let data = readData(filepath);
+    let fd = fs.openSync(filepath, "w");
+    fs.writeFile(fd, data, ()=>{
+        fs.closeSync(fd);
+    });
+};
+
+let filepath1 = './input.txt';
+let filepath2 = './output.txt';
+
+let data = readData(filepath1);
 
 for (let i=0; i<data.length;i++)
     console.log(i + ' ' + data[i]);
 
+writeData(filepath2,data[1]);
