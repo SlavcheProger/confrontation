14d042aa04ae9eaa3e5348847864aaf3488a240a
Vyacheslav Feofanov <slkf72@yandex.ru>
2018-04-04 19:41:15 +0700

    Added data parsing

--- app.js
+++ app.js
@@ -27,17 +27,27 @@ process.stdin.on('data', function (chunk) {
   paths = chunk.toString().split(' ');
   //////
 let obj = {
-    input: paths[0],
-    output: paths[1],
-    gameType:0,
-    rescount:0,
-    armyCount:0,
-    points:0
+    input: paths[0], //путь к input файлу
+    output: paths[1], //путь к output файлу
+    gameType:0, //тип игры
+    resCount:0, //кол-во ресурсов
+    armyCount:0, //кол-во армий
+    points:0, //кол-во очков за выйгрыш
+    strokeNum: 0, //номер хода
+    enemyRes: 0 //распределение ресурсов противника
     };
 
 console.log('inp: ' + obj.input);
 
-
+let data = readData(obj.input);
+console.log(data[0]);
+let spdata = data[0].split(' ');
+obj.gameType = spdata [0];
+obj.resCount = spdata [1];
+obj.armyCount = spdata [2];
+obj.points = spdata [3];
+obj.strokeNum = data [1];
+obj.enemyRes = data [2];
   /////
   process.exit();
 });
